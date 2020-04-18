e4d6e85f345949da6c3cead425170cb089944641
Wickie Lee <wickie.leejr@gmail.com>
2020-04-18 01:12:46 -0400

    orient clock along z axis

--- excercises/analog.c
+++ excercises/analog.c
@@ -14,20 +14,20 @@ struct Clock {
 
 int main() {
     struct Clock clock = {
-        .origin = point(25, 0, 25),
-        .hour = point(0, 0, 1)
+        .origin = point(25, 25, 0),
+        .hour = point(0, 1, 0)
     };
 
     Canvas *c = canvas(50, 50);
     Tuple pixel_color = color(1, 1, 1);
 
-    Matrix *m = rotation_y(M_PI/6);
+    Matrix *m = rotation_z(M_PI/6);
     double radius = c->width * (3.0/8.0);
 
     for (int i = 0; i < 12; i++) {
         Tuple loc = mscale(clock.hour, radius);
         loc = add(loc, clock.origin);
-        write_pixel(c, x(loc), z(loc), pixel_color);
+        write_pixel(c, x(loc), y(loc), pixel_color);
         clock.hour = multiply_matrix_with_tuple(m, clock.hour);
     }
 
