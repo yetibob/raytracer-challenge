034fc16919a675ef028045a5d4e427b984bff6ae
Wickie Lee <wickie.leejr@gmail.com>
2020-06-18 00:38:38 -0400

    update with tupleage

--- excercises/shadow.c
+++ excercises/shadow.c
@@ -15,29 +15,37 @@ int main() {
     double half = wall_size / 2;
 
     Canvas *c = canvas(canvas_pixels, canvas_pixels);
-    Tuple ray_origin = tuple_point(0, 0, -5);
+    Tuple ray_origin = {0, 0, -5};
+    tuple_point(ray_origin);
 
-    Sphere *s = ray_sphere();
-    Tuple hit_pixel = color(1, 0, 0);
+    Sphere s;
+    ray_sphere_init(&s);
+    Tuple hit_pixel = {1, 0, 0};
     int count;
 
     for (int y = 0; y < c->height; y++) {
         double world_y = half - pixel_size * y;
         for (int x = 0; x < c->width; x++) {
             double world_x = -half + pixel_size * x;
-            Tuple pos = tuple_point(world_x, world_y, wall_z);
-            Tuple tmp = ztuple();
-            memcpy(tmp, ray_origin, sizeof(double)*4);
-            Tuple dir = tuple_subtract(pos, tmp);
-            Tuple norm_dir  = tuple_normalize(dir);
-            Ray *r = ray(tmp, norm_dir);
-            Intersection **xs = ray_intersect(s, r, &count);
+            Tuple pos = {world_x, world_y, wall_z};
+            tuple_point(pos);
+            Tuple tmp = {0};
+            memcpy(tmp, ray_origin, sizeof(double)*TUPLE_LEN);
+
+            Tuple dir;
+            tuple_subtract(pos, tmp, dir);
+
+            Tuple norm_dir;
+            tuple_normalize(dir, norm_dir);
+
+            Ray r;
+            memcpy(r.origin, tmp, sizeof(double)*TUPLE_LEN);
+            memcpy(r.direction, norm_dir, sizeof(double)*TUPLE_LEN);
+
+            Intersection **xs = ray_intersect(&s, &r, &count);
             if (ray_hit(xs, count) != NULL) {
                 canvas_write(c, x, y, hit_pixel);
             } 
-            tuple_destroy(pos);
-            tuple_destroy(dir);
-            ray_destroy(r);
             for (int i = 0; i < count; i++) {
                 ray_intersection_destroy(xs[i]);
             }
