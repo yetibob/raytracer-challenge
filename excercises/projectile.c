2e142dbdc6b9dd3f137d853af654df9c6db9a820
Wickie Lee <wickie.leejr@gmail.com>
2020-06-18 00:24:45 -0400

    update with new tuple style

--- excercises/projectile.c
+++ excercises/projectile.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "../src/tuples.h"
 #include "../src/canvas.h"
 #include "../src/colors.h"
@@ -16,23 +17,37 @@ struct Environment {
 };
 
 void tick(struct Environment env, struct Projectile *proj, Tuple add) {
-    Tuple freePos = proj->position;
-    Tuple freeVol = proj->velocity;
+    Tuple tmp;
+    tuple_add(proj->position, proj->velocity, tmp);
+    memcpy(proj->position, tmp, sizeof(double) * TUPLE_LEN);
     
-    proj->position = tuple_add(proj->position, proj->velocity);
-    proj->velocity = tuple_add(proj->velocity, add);
-
-    free(freePos);
-    free(freeVol);
+    tuple_add(proj->velocity, add, tmp);
+    memcpy(proj->velocity, tmp, sizeof(double) * TUPLE_LEN);
 }
 
 int main() {
-    struct Projectile p = { tuple_point(0, 1 ,0), tuple_scale(tuple_normalize(tuple_vector(1, 1.8, 0)), 11.25) };
-    struct Environment e = { tuple_vector(0, -0.1, 0), tuple_vector(-0.01, 0, 0) };
-    Tuple add = tuple_add(e.gravity, e.wind);
+    Tuple s = {1, 1.8, 0};
+    tuple_vector(s);
+
+    Tuple norm;
+    tuple_normalize(s, norm);
+
+    Tuple scaled;
+    tuple_scale(norm, scaled, 11.25);
+
+    struct Projectile p = { .position = {0, 1 ,0}};
+    memcpy(p.velocity, scaled, sizeof(double) * TUPLE_LEN);
+    tuple_point(p.position);
+    tuple_vector(p.velocity);
+
+    struct Environment e = { .gravity={0, -0.1, 0}, .wind={-0.01, 0, 0} };
+    tuple_vector(e.gravity);
+    tuple_vector(e.wind);
+    Tuple add;
+    tuple_add(e.gravity, e.wind, add);
     Canvas *c = canvas(900, 550);
     char *ppm;
-    Tuple pixel_color = color(0, 1, 0);
+    Tuple pixel_color = {0, 1, 0};
 	
     while(tuple_y(p.position) > 0) {
         tick(e, &p, add);
