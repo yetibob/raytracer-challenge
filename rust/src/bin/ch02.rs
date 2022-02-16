4dae7fed07cab35acb618b2d4f749a7596761e9f
Wickie Lee <wickie.leejr@protonmail.com>
2022-02-15 23:03:52 -0500

    ignore ppms and start ch02 exercise

---
+++ rust/src/bin/ch02.rs
@@ -0,0 +1,46 @@
+use std::fs;
+
+use rust::{
+    tuple::Tuple,
+    color::Color,
+    canvas::Canvas,
+};
+
+struct Projectile {
+    position: Tuple,
+    velocity: Tuple,
+}
+
+struct Environment {
+    gravity: Tuple,
+    wind: Tuple,
+}
+
+fn main() {
+    let mut p = Projectile {
+        position: Tuple::point(0.0, 1.0, 0.0),
+        velocity: Tuple::vector(1.0, 1.0, 0.0).normalize(),
+    };
+
+    let e = Environment {
+        gravity: Tuple::vector(0.0, -0.1, 0.0),
+        wind: Tuple::vector(-0.01, 0.0, 0.0),
+    };
+
+    let mut c = Canvas::new(1920, 1080);
+    let col = Color::new(1.0, 0.0, 0.0);
+
+    while p.position.y > 0.0 {
+        p = tick(&e, &p); 
+        c.write_pixel(p.position.x as u32, p.position.y as u32, col);
+    }
+
+    fs::write("tmp.ppm", c.to_ppm()).expect("Uh Oh!");
+}
+
+fn tick(env: &Environment, proj: &Projectile) -> Projectile {
+    Projectile {
+        position: proj.position + proj.velocity,
+        velocity: proj.velocity + env.gravity + env.wind,
+    }
+}
