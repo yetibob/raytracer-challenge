807eb9ac6f0b903d44fc0e91558f5485d277822a
Wickie Lee <wickie.leejr@protonmail.com>
2022-03-12 20:32:02 -0500

    modify ch04 camera to look down z axis

--- rust/src/bin/ch04.rs
+++ rust/src/bin/ch04.rs
@@ -11,19 +11,19 @@ use rust::{
 };
 
 fn main() {
-    let mut c = Canvas::new(100, 100);
+    let mut c = Canvas::new(500, 500);
     let col = Color::new(1.0, 1.0, 1.0);
 
-    let origin = Tuple::point(c.width as f64 / 2.0, 0.0, c.height as f64 / 2.0);
-    let twelve = Tuple::point(0.0, 0.0, 1.0);
+    let origin = Tuple::point(c.width as f64 / 2.0, c.height as f64 / 2.0, 0.0);
+    let twelve = Tuple::point(0.0, 1.0, 0.0);
 
     let clock_radius = c.width as f64 * (3.0 / 8.0);
 
     for i in 1..13 {
-        let rot_y = Matrix::rotation_y(i as f64 * (PI / 6.0));
+        let rot_y = Matrix::rotation_z(i as f64 * (PI / 6.0));
         let mut point = &rot_y * &twelve; 
         point = (point * clock_radius) + origin; 
-        c.write_pixel(point.x as u32, c.height - point.z as u32, col);
+        c.write_pixel(point.x as u32, c.height - point.y as u32, col);
     }
     
 
